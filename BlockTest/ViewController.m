bc9a401650283821a859f555c5c695c8a603bd25
hnny09 <643496623@qq.com>
2016-01-05 22:46:47 +0800

    homeSecoder

--- BlockTest/ViewController.m
+++ BlockTest/ViewController.m
@@ -16,9 +16,7 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    
+    [super viewDidLoad];    
 }
 
 
